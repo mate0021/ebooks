a23438e747f884f4ae2c9b26d2e5015b5417201c
mate00 <mami.ext@gmail.com>
2018-01-19 22:29:26 +0100

    Spring boot init project.

---
+++ src/main/java/rnd/mate00/ebooks/EbooksApplication.java
@@ -0,0 +1,12 @@
+package rnd.mate00.ebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbooksApplication.class, args);
+	}
+}
---
+++ src/test/java/rnd/mate00/ebooks/EbooksApplicationTests.java
@@ -0,0 +1,16 @@
+package rnd.mate00.ebooks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EbooksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
