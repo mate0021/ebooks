b5730f89d56a835c6ae88decb36072ed730d1f6b
mate00 <mami.ext@gmail.com>
2018-12-09 17:17:49 +0100

    Use shopping service class.

--- src/main/java/rnd/mate00/ebooks/controllers/PurchaseController.java
+++ src/main/java/rnd/mate00/ebooks/controllers/PurchaseController.java
@@ -1,13 +1,18 @@
 package rnd.mate00.ebooks.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import rnd.mate00.ebooks.commands.PurchaseCommand;
+import rnd.mate00.ebooks.service.ShoppingService;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
+
+import static java.time.ZoneId.systemDefault;
 
 /**
  * Created by mate00 on 09.12.18.
@@ -15,11 +20,18 @@ import javax.validation.Valid;
 @Controller
 public class PurchaseController {
 
+    @Autowired
+    private ShoppingService shoppingService;
+
+
     @PostMapping
     @RequestMapping("/buyBook")
-    public String onBuyBook(@Valid @ModelAttribute(name = "purchase") PurchaseCommand purchaseCommand,
+    public String onBuyBook(@Valid @ModelAttribute(name = "purchase") PurchaseCommand purchaseBean,
                           BindingResult bindingResult) {
-        System.out.println(purchaseCommand);
+        System.out.println(purchaseBean);
+
+        LocalDate buyDate = purchaseBean.getBuyDate().toInstant().atZone(systemDefault()).toLocalDate();
+        shoppingService.buyABook(purchaseBean.getReader(), purchaseBean.getBook(), purchaseBean.getShop(), purchaseBean.getPrice(), buyDate);
 
         return "redirect:/books";
     }
