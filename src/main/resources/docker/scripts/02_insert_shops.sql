f84793520f655125220e4c7787ce1bacec5edf8b
mate00 <mami.ext@gmail.com>
2019-04-16 07:40:41 +0200

    Load of book data.

--- src/main/resources/docker/scripts/02_insert_shops.sql
+++ src/main/resources/docker/scripts/02_insert_shops.sql
@@ -2,3 +2,5 @@ insert into shop(name, url) values ('Virtualo', 'www.virtualo.pl');
 insert into shop(name, url) values ('Ebookpoint', 'www.ebookpoint.pl');
 insert into shop(name, url) values ('Publio', 'www.publio.pl');
 insert into shop(name, url) values ('Woblink', 'www.woblink.com');
+insert into shop(name, url) values ('Świat książki', 'www.swiatksiazki.com');
+insert into shop(name, url) values ('Empik', 'www.empik.com');
