275fc7a0c10d5660f4f30ca2d47d542f3e70b77d
mate00 <mami.ext@gmail.com>
2019-05-09 07:18:39 +0200

    fiddling with db init in test

---
+++ src/main/java/rnd/mate00/ebooks/config/DatabaseConfiguration.java
@@ -0,0 +1,43 @@
+package rnd.mate00.ebooks.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.password}")
+    private String pass;
+
+    @Bean()
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(pass);
+
+        System.out.println(" !@# " + driver);
+        System.out.println(" !@# " + user);
+
+        return dataSource;
+    }
+
+}
