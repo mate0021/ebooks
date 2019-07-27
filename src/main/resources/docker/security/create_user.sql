c2606d305d2e00023f55f169f6823385e01101b7
mate00 <mami.ext@gmail.com>
2019-07-27 15:08:04 +0200

    Updated MySQL script for adding prod user.

--- src/main/resources/docker/security/create_user.sql
+++ src/main/resources/docker/security/create_user.sql
@@ -1,5 +1,16 @@
 use sec;
 
+drop table if exists role;
+
+create table role (
+    id bigint auto_increment,
+    role_name varchar(255),
+    PRIMARY KEY (`id`)
+);
+
+insert into role (id, role_name) values (1, 'admin');
+insert into role (id, role_name) values (2, 'reader');
+
 drop table if exists users;
 
 create table users (
@@ -12,12 +23,14 @@ create table users (
 
 insert into users (username, password, enabled) values ('mate00', '$2a$10$sGx1EawJhzFIaYJHLDSs5ewQqzCO6M6jGAZnMcw6RUAsuAwe2CUMK', true);
 
-drop table if exists authorities;
 
-create table authorities (
-    username varchar(255),
-    authority varchar(255),
-    UNIQUE (username, authority)
+drop table if exists users_roles;
+
+create table users_roles (
+    users_id bigint,
+    roles_id bigint
 );
 
-insert into authorities (username, authority) values ('mate00', 'admin');
+insert into users_roles (users_id, roles_id) values (1, 1);
+insert into users_roles (users_id, roles_id) values (1, 2);
+
