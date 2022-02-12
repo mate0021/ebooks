ff754046548d67b241b43b5ab7ff1552e943a295
mate0021 <mami.ext@gmail.com>
2022-02-12 10:27:29 +0100

    Update 04_add_books.sql

--- src/main/resources/docker/scripts/04_add_books.sql
+++ src/main/resources/docker/scripts/04_add_books.sql
@@ -106,3 +106,4 @@ insert into book (author, locations, title, theme_id) values ('Jesper Juul', 0, 
 insert into book (author, locations, title, theme_id) values ('David Lagercrantz', 7037, 'Co nas nie zabije', 6);
 insert into book (author, locations, title, theme_id) values ('David Lagercrantz', 5604, 'Mężczyzna, który gonił swój cień', 6);
 insert into book (author, locations, title, theme_id) values ('Brian W. Kernighan', 3990, 'Jak Unix tworzył historię', 5);
+insert into book (author, locations, title, theme_id) values ('Ken Follett', 6799, 'Lot ćmy', 6);
