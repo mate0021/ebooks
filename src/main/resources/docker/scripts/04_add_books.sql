522efc999b421e7ca24adb9205f4cbc38ee969ee
mate0021 <mami.ext@gmail.com>
2022-11-28 11:07:58 +0100

    Update 04_add_books.sql

--- src/main/resources/docker/scripts/04_add_books.sql
+++ src/main/resources/docker/scripts/04_add_books.sql
@@ -118,3 +118,4 @@ insert into book (author, locations, title, theme_id) values ('Ilona Wiśniewska
 insert into book (author, locations, title, theme_id) values ('Andrea Camilleri', 3941, 'Głosy nocy', 7);
 insert into book (author, locations, title, theme_id) values ('Krzysztof Kotowski', 2862, 'Krew snajperów', 1);
 insert into book (author, locations, title, theme_id) values ('Przemysław Waingertner', 3507, 'Czwarta stolica. Kiedy Łódź rządziła Polską', 2);
+insert into book (author, locations, title, theme_id) values ('Jacek Komuda', 8390, 'Galeony wojny', 8);
