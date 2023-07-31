25957f1e90ee64dcc4df74959a21aa7393208143
mate0021 <mami.ext@gmail.com>
2023-07-31 09:51:40 +0200

    Update 04_add_books.sql

--- src/main/resources/docker/scripts/04_add_books.sql
+++ src/main/resources/docker/scripts/04_add_books.sql
@@ -132,3 +132,4 @@ insert into book (author, locations, title, theme_id) values ('Jacek Komuda', 66
 insert into book (author, locations, title, theme_id) values ('Sid Meier', 4572, 'Wspomnienia. Oficjalna autobiografia twórcy Cywilizacji', 5);
 insert into book (author, locations, title, theme_id) values ('Chris Riccomini, Dmitriy Ryaboy', 246, 'Brakujący plik Readme', 5);
 insert into book (author, locations, title, theme_id) values ('Piotr Kościelny', 5636, 'Dom', 7);
+insert into book (author, locations, title, theme_id) values ('Krystyna Kurczab-Redlich', 10525, 'Głową o mur Kremla', 1);
