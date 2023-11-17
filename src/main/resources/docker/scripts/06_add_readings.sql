8208c17413505b8c3c3733f0a26da89088fdd520
mate0021 <mami.ext@gmail.com>
2023-11-17 11:32:13 +0100

    Update 06_add_readings.sql

--- src/main/resources/docker/scripts/06_add_readings.sql
+++ src/main/resources/docker/scripts/06_add_readings.sql
@@ -116,3 +116,4 @@ insert into reading_progress (reading_start, reading_end, key_book, key_reader) 
 insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2023/10/27', '2023/11/03', 139, 1);
 insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2023/11/03', '2023/11/12', 142, 1);
 insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2023/11/13', null, 144, 1);
+insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2023/11/17', null, 145, 1);
