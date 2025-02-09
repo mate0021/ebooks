20d46ffc90955a64fea2181082ecc690cde02d1b
mate0021 <mami.ext@gmail.com>
2025-02-09 20:55:46 +0100

    Update 04_add_books.sql

--- src/main/resources/docker/scripts/04_add_books.sql
+++ src/main/resources/docker/scripts/04_add_books.sql
@@ -159,4 +159,4 @@ insert into book (author, locations, title, theme_id) values ('Alicja Grzesiak',
 insert into book (author, locations, title, theme_id) values ('Igor Ryżow', 3248, 'Negocjacje z potworami. Jak osiągnąć to, co chcemy negocjując z silniejszymi od siebie', 13);
 insert into book (author, locations, title, theme_id) values ('Sławek Gortych', 5731, 'Schronisko, które przestało istnieć', 7);
 insert into book (author, locations, title, theme_id) values ('David Perlmutter, Kristin Loberg', 8657, 'Mózg na zbożowym detoksie. Zaskakująca prawda o pszenicy, węglowodanach i cukrze - cichych zabójcach Twojego mózgu', 17);
-
+insert into book (author, locations, title, theme_id) values ('Marcin Mortka', 5202, 'Nie ma tego złego', 8);
