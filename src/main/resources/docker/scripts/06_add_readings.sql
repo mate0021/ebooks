9c74174524e84f7bd1783da6c821e984758596db
mate0021 <mami.ext@gmail.com>
2026-01-07 10:52:39 +0100

    Add new reading progress entry for December 2025

--- src/main/resources/docker/scripts/06_add_readings.sql
+++ src/main/resources/docker/scripts/06_add_readings.sql
@@ -140,4 +140,6 @@ insert into reading_progress (reading_start, reading_end, key_book, key_reader) 
 insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2025/09/08', '2025/10/01', 171, 1);
 insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2025/11/06', '2025/11/03', 173, 1);
 insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2025/11/03', null, 174, 1);
+insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2025/12/26', null, 179, 1);
+
 
