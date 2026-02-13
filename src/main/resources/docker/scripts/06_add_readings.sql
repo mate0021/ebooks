fb2972879d6589ba2204d336cc8a35e37d177f1a
mate0021 <mami.ext@gmail.com>
2026-02-13 15:45:34 +0100

    Update reading_progress entries with new data

--- src/main/resources/docker/scripts/06_add_readings.sql
+++ src/main/resources/docker/scripts/06_add_readings.sql
@@ -142,5 +142,6 @@ insert into reading_progress (reading_start, reading_end, key_book, key_reader) 
 insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2025/11/03', null, 174, 1);
 insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2025/12/26', '2026/01/09', 179, 1);
 insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2026/01/09', '2026/01/21', 184, 1);
-insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2026/01/21', null, 187, 1);
+insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2026/01/21', '2026/02/04', 187, 1);
+insert into reading_progress (reading_start, reading_end, key_book, key_reader) values ('2026/02/04', null, 180, 1);
 
