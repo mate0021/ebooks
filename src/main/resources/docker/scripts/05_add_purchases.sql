66b74384cfb0d64c794632d1a7b50cf3f425c128
mate0021 <mami.ext@gmail.com>
2026-07-01 18:21:01 +0200

    Update 05_add_purchases.sql

--- src/main/resources/docker/scripts/05_add_purchases.sql
+++ src/main/resources/docker/scripts/05_add_purchases.sql
@@ -178,3 +178,5 @@ insert into shopping (buy_date, price, key_book, key_reader, key_shop) values ('
 insert into shopping (buy_date, price, key_book, key_reader, key_shop) values ('2026/04/06', 24.21, 194, 1, 2);
 insert into shopping (buy_date, price, key_book, key_reader, key_shop) values ('2026/04/06', 16.11, 195, 1, 2);
 insert into shopping (buy_date, price, key_book, key_reader, key_shop) values ('2026/06/18', 8.91, 197, 1, 2);
+insert into shopping (buy_date, price, key_book, key_reader, key_shop) values ('2026/06/29', 8.91, 198, 1, 2);
+insert into shopping (buy_date, price, key_book, key_reader, key_shop) values ('2026/06/30', 8.91, 199, 1, 2);
